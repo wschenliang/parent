c94c149a197e4a3a2073928e14fd48fd397ba820
chenliang <chenliang@1haitao.com>
2020-02-21 13:17:23 +0800

    修改公司静态页面

---
+++ manage-model/src/main/java/com/rongqi/manage/pojo/CompanyDetail.java
@@ -0,0 +1,24 @@
+package com.rongqi.manage.pojo;
+
+import java.io.Serializable;
+
+/**
+ * @author chenliang
+ * @version 1.0.0
+ * Modification  History:
+ * Date         Author        Version        Description
+ * ------------------------------------------------------
+ * 2020-1-6   chenliang     1.0.0          create
+ */
+public class CompanyDetail implements Serializable {
+    private static final long serialVersionUID = 820888937612392147L;
+
+    private String companyName;
+
+    private String companyDescr;
+
+
+
+
+
+}
